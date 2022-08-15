a0a7476763275557204581c74cfe1b83e62c20a9
Denis Bernovich <denisbernovich@gmail.com>
2022-08-15 17:22:29 +0300

    feat: completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min;
+        this.max;
+        this.result;
+      }
+    
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
+      }
+    
+      guess() {
+        this.result = Math.round((this.max - this.min) / 2) + this.min;
+        return this.result;
+      }
+    
+      lower() {
+        this.max = this.result;
+      }
+    
+      greater() {
+        this.min = this.result;
+      }
     }
 
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
-}
 
 module.exports = GuessingGame;
+
+
